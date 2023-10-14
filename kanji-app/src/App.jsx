8ac86a5e5dfd8c46ff5af0f6bc496361cbfdfc6b
Greg Johnston <gregmj.sco@gmail.com>
2023-10-14 14:44:01 +0900

    Add sample wrapper code to access the Kanji API

--- kanji-app/src/App.jsx
+++ kanji-app/src/App.jsx
@@ -1,12 +1,31 @@
 import './App.css'
 
-function App() {
+const { Kanjiapi } = require('kanjiapi-wrapper')
 
-  return (
-    <>
-      <h1>React Kanji app</h1>
+const Kanji = ({ kanji }) =>
+    kanji.status === Kanjiapi.SUCCESS ?
+        <ul>
+            {kanji.value.meanings.map(meaning =>
+                <li key={meaning}>{meaning}</li>)}
+        </ul> :
+        kanji.status === Kanjiapi.LOADING ?
+        <div>Loading</div> :
+        <div>Error</div>
+
+const App = ({ kanjiapi }) => {
+    const [timestamp, setTimestamp] = React.useState(Date.now())
+    kanjiapi.addListener('appState', () => setTimestamp(Date.now()))
+
+    const [character, setCharacter] = React.useState('字')
+
+    return <>
+        <input
+            type="text"
+            value={character}
+            onChange={e => setCharacter(e.target.value)}/>
+        <Kanji kanji={kanjiapi.getKanji(character)}/>
     </>
-  )
 }
 
-export default App
+this.kanjiapi = Kanjiapi.build()
+ReactDOM.render(<App kanjiapi={kanjiapi}/>, document.getElementById('app'))
