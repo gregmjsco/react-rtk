5f318736818565a73e1460127304e758dcaa194c
Greg Johnston <gregmj.sco@gmail.com>
2023-10-14 17:51:20 +0900

    Remove kanjidev wrapper code

--- kanji-app/src/App.jsx
+++ kanji-app/src/App.jsx
@@ -1,31 +1,12 @@
 import './App.css'
 
-const { Kanjiapi } = require('kanjiapi-wrapper')
+function App() {
 
-const Kanji = ({ kanji }) =>
-    kanji.status === Kanjiapi.SUCCESS ?
-        <ul>
-            {kanji.value.meanings.map(meaning =>
-                <li key={meaning}>{meaning}</li>)}
-        </ul> :
-        kanji.status === Kanjiapi.LOADING ?
-        <div>Loading</div> :
-        <div>Error</div>
-
-const App = ({ kanjiapi }) => {
-    const [timestamp, setTimestamp] = React.useState(Date.now())
-    kanjiapi.addListener('appState', () => setTimestamp(Date.now()))
-
-    const [character, setCharacter] = React.useState('字')
-
-    return <>
-        <input
-            type="text"
-            value={character}
-            onChange={e => setCharacter(e.target.value)}/>
-        <Kanji kanji={kanjiapi.getKanji(character)}/>
+  return (
+    <>
+      <h1>React Kanji app</h1>
     </>
+  )
 }
 
-this.kanjiapi = Kanjiapi.build()
-ReactDOM.render(<App kanjiapi={kanjiapi}/>, document.getElementById('app'))
+export default App
