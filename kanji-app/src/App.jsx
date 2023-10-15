fde03cbc4560b99b7cab73b439379dc51cf6805c
Greg Johnston <gregmj.sco@gmail.com>
2023-10-15 11:30:16 +0900

    List out Heisig kanji on page

--- kanji-app/src/App.jsx
+++ kanji-app/src/App.jsx
@@ -1,18 +1,33 @@
 import './App.css'
+import { useState, useEffect } from 'react';
 
-fetch('https://kanjiapi.dev/v1/kanji/heisig')
-  .then(function(response) {
-    return response.json();
-  })
-  .then(function (response) {
-    console.log(response);
-  })
+
+
 
 function App() {
+
+const [items, setItems] = useState([]);
+const [mode, setMode] = useState('');
+
+useEffect(() => {
+
+  fetch('https://kanjiapi.dev/v1/kanji/heisig')
+    .then(function(response) {
+      return response.json();
+    })
+    .then(function (response) {
+      setItems(response);
+    })
+})
+
 
   return (
     <>
       <h1>React Kanji app</h1>
+      <button onClick={() => setMode('Memory Game')}>Memory Game</button>
+      {items.map( item => {
+        return <pre>{JSON.stringify(item)}</pre>
+      })}
     </>
   )
 }
