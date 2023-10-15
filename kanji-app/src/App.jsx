79a75e191fffd1de1338179d59fc4c33308c3b3a
Greg Johnston <gregmj.sco@gmail.com>
2023-10-15 17:56:59 +0900

    Able to get list of kanji and display

--- kanji-app/src/App.jsx
+++ kanji-app/src/App.jsx
@@ -7,18 +7,20 @@ import { useState, useEffect } from 'react';
 function App() {
 
 const [items, setItems] = useState([]);
+const [kanji, setKanji] = useState([]);
 const [mode, setMode] = useState('');
 
 useEffect(() => {
 
-  fetch('https://kanjiapi.dev/v1/kanji/heisig')
+  fetch('https://kanjiapi.dev/v1/kanji/grade-1')
     .then(function(response) {
       return response.json();
     })
     .then(function (response) {
       setItems(response);
+      console.log(response)
     })
-})
+}, [])
 
 
   return (
@@ -26,7 +28,16 @@ useEffect(() => {
       <h1>React Kanji app</h1>
       <button onClick={() => setMode('Memory Game')}>Memory Game</button>
       {items.map( item => {
+        fetch(`https://kanjiapi.dev/v1/kanji/${item}`)
+        .then(function (response) {
+          return response.json();
+        })
+        .then(function(response) {
+          console.log(response);
+          setKanji(response);
+        })
         return <pre>{JSON.stringify(item)}</pre>
+        
       })}
     </>
   )
