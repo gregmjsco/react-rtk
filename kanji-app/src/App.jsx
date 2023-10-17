bd44d75cf8ac1b181e6c254c0d0b17d5006c953b
Greg Johnston <gregmj.sco@gmail.com>
2023-10-17 22:03:07 +0900

    Remove API test code and start making components for memory card game

--- kanji-app/src/App.jsx
+++ kanji-app/src/App.jsx
@@ -1,6 +1,7 @@
 import './App.css'
 import { useState, useEffect } from 'react';
 import kanjiData from '../rtk_chapters/rtkChapter1.json';
+import Card from './components/Card';
 
 
 
@@ -35,30 +36,7 @@ useEffect(() => {
   return (
     <>
       <h1>React Kanji app</h1>
-      <button onClick={() => setMode('Memory Game')}>Memory Game</button>
-      {items.map( item => {
-        fetch(`https://kanjiapi.dev/v1/kanji/${item}`)
-        .then(function (response) {
-          return response.json();
-        })
-        .then(function(response) {
-          console.log(response);
-          setKanji(response);
-        })
-        return <pre>{JSON.stringify(item)}</pre>
-        
-      })}
-
-    <div>
-      <h1>Kanji List</h1>
-      <ul>
-        {kanjiList.map((kanji, index) => (
-          <li key={index}>
-            Kanji: {kanji.kanji}, Keyword: {kanji.keyword}
-          </li>
-        ))}
-      </ul>
-    </div>  
+      <Card />
     </>
   )
 }
