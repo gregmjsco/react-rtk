f8a44481a56588edae1188a57b53504f8334a417
Greg Johnston <gregmj.sco@gmail.com>
2023-10-18 19:46:14 +0900

    Not showing kanji but able to log data passed to Card component and kanji data is being passed

--- kanji-app/src/components/Card.jsx
+++ kanji-app/src/components/Card.jsx
@@ -2,9 +2,10 @@ import React from "react";
 import "./Card.css"
 
 function Card({ kanji }) {
+    console.log("Kanji: ", kanji)
     return (
         <div className="kanji-card">
-            <p>{kanji.kanji}</p>
+            <p>{kanji.kanji[0]}</p>
         </div>
     )
 }
